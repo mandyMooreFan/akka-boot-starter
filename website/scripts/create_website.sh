08e5a18c363a2869ce9f252743d54cb8b07617cc
Mandymooerfan <markbeeb@gmail.com>
2017-12-15 10:59:27 -0500

    Updated create website to remove old folder and added files to git for docker image.

--- website/scripts/create_website.sh
+++ website/scripts/create_website.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+echo 'Starting to create website'
 cd ..
+rm -rf ui
+echo 'Removed old UI folder, starting clone of React Boilerplate'
 git clone --depth=1 https://github.com/react-boilerplate/react-boilerplate.git
 mv react-boilerplate ui
 cd ui
-
+echo 'Clone Complete, running NPM setup'
 npm run setup
+echo 'Finished.'
 
 
